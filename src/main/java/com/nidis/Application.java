5b82ab1d75b04be3f1ac70412e1bde7461a5f18d
symeonal <symeonal@g.com>
2018-07-30 18:18:49 +0300

    setup

--- src/main/java/com/nidis/Application.java
+++ src/main/java/com/nidis/Application.java
@@ -1,7 +1,10 @@
+package com.nidis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class Application {
 
